fbcf6fe26d151f06779f6e7112209514ecac933e
Amit kukreja <thenameisamitkukreja@gmail.com>
2024-09-18 21:14:29 +0530

    Clean-Up

---
+++ CHP2/DataTypes/PrimitiveData.java
@@ -0,0 +1,21 @@
+package CHP2.DataTypes;
+
+public class PrimitiveData {
+    public static void main(String[] args) {
+        byte a = 30;
+        int b = 271;
+        float pi = 3.14F;
+        Long c = 12345678900L;
+        double d = 123.456;
+        boolean e = true;
+        char f = 'A';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(pi);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        Sytem.out.println(f);
+
+    }
+}
